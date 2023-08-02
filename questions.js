02c4609e02fcd92bbb28c12983cb634b6ab9f912
Najmi Aziz <najmiabo@gmail.com>
2023-08-02 17:14:40 +0700

    quiz done

---
+++ questions.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Kapan Indonesia merdeka?",
+        answer: "B. 17 Agustus 1945",
+        options: [
+            "A. 7 Agustus 1765",
+            "B. 17 Agustus 1945",
+            "C. 8 Oktober 1964",
+            "D. 10 November 1995"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Siapa yang membacakan teks proklamasi?",
+        answer: "D. IR Soekarno",
+        options: [
+            "A. Jendral Soedirman",
+            "B. Bung Tomo",
+            "C. Ki Hajar Dewantara",
+            "D. IR Soekarno"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Siapa yang menjahit bendera merah putih?",
+        answer: "A. Fatmawati",
+        options: [
+            "A. Fatmawati",
+            "B. Fatimah",
+            "C. Raden Ajeng Kartini",
+            "D. Cut Nyak Dhien"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Berapa jumlah Provinsi setelah Indonesia merdeka?",
+        answer: "B. 8 Provinsi",
+        options: [
+            "A. 4 Provinsi",
+            "B. 8 Provinsi",
+            "C. 23 Provinsi",
+            "D. 33 Provinsi"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Siapa pencipta lagu Indonesia Raya?",
+        answer: "C. W.R Soepratman",
+        options: [
+            "A. Ahmad Dhani",
+            "B. Moh Hatta",
+            "C. W.R Soepratman",
+            "D. Aldi Taher"
+        ]
+    }
+]
+
+
