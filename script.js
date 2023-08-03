b0602c44878c8ead9b4870f1ada591eafafbd639
Najmi Aziz <najmiabo@gmail.com>
2023-08-03 18:50:15 +0700

    gw gatau cara masukin foto ke javascript

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ const exitTodoListBtn = document.querySelector('.exit-todoList-btn');
 const donateUs = document.querySelector('.donate-us');
 const popupDonateUs = document.querySelector('.popup-donateUs');
 const exitDonateUsBtn = document.querySelector('.exit-donateUs-btn');
+const merchandise = document.querySelector('.merchandise');
+const popupMerchandise = document.querySelector('.popup-merchandise');
+const exitMerchandiseBtn = document.querySelector('.exit-merchandise-btn');
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active');
@@ -236,4 +239,105 @@ donateUs.onclick = () => {
 exitDonateUsBtn.onclick = () => {
     popupDonateUs.classList.remove('active');
     main.classList.remove('active');
+}
+
+merchandise.onclick = () => {
+    main.classList.add('active');
+    popupMerchandise.classList.add('active');
+}
+
+exitMerchandiseBtn.onclick = () => {
+    popupMerchandise.classList.remove('active');
+    main.classList.remove('active');
+}
+
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let lists = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('.body-merchandise');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'PRODUCT NAME 1',
+        image: 'images/1.PNG',
+        price: 120000
+    },
+    {
+        id: 2,
+        name: 'PRODUCT NAME 2',
+        image: 'images/2.PNG',
+        price: 120000
+    },
+    {
+        id: 3,
+        name: 'PRODUCT NAME 3',
+        image: 'images/3.PNG',
+        price: 220000
+    }
+];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        lists.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="image/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice.toLocaleString();
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
 }
